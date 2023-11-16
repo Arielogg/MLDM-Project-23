0de0f58cc31dce144cadc6f030c3af764778df22
fcortesj <131784473+fcortesj98@users.noreply.github.com>
2023-11-16 13:49:58 +0100

    feature/adding_resuts_pde_deformation_method
    Adding images, slight changes in the pde extracton script and the results ina markdown file.

--- source/pde_discovery/PDE_generation.py
+++ source/pde_discovery/PDE_generation.py
@@ -18,7 +18,11 @@ import numpy as np
 #features_dir = r"../../generations/changemap_generations/cropped_resized/24"
 #features_dir = r"../../generations/changemap_generations/cropped_cropped/20"
 #features_dir = r"../../generations/changemap_generations/cropped_cropped/22"
-features_dir = r"../../generations/changemap_generations/cropped_cropped/24"
+#features_dir = r"../../generations/changemap_generations/cropped_cropped/24"
+
+""" Features directory of the generated registered by deformation (This images are .jpg)"""
+#features_dir = r"../../generations/deformation_generations/registered/24"
+features_dir = r"../../generations/deformation_generations/deformations/24"
 
 
 #Only available for the brandao generator, timesteps
