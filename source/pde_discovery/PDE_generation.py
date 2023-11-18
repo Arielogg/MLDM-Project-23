7ad8d5225cb3af0afa9266736d24bd04951ac42f
bastis007 <schaefer.bastian@mail.de>
2023-11-18 17:42:38 +0100

    Adapt changemap_generation.ipynb to the multitrajectories, generate new data and adapt PDE_generation.py

--- source/pde_discovery/PDE_generation.py
+++ source/pde_discovery/PDE_generation.py
@@ -13,16 +13,12 @@ import numpy as np
 #features_dir = r'brandao_generations' #Brandao et al. simulator, epsilon 0.3 gamma 0.2
 
 """ Features directory of the generated changemaps """
-#features_dir = r"../../generations/changemap_generations/cropped_resized/20"
-#features_dir = r"../../generations/changemap_generations/cropped_resized/22"
-#features_dir = r"../../generations/changemap_generations/cropped_resized/24"
-#features_dir = r"../../generations/changemap_generations/cropped_cropped/20"
-#features_dir = r"../../generations/changemap_generations/cropped_cropped/22"
-#features_dir = r"../../generations/changemap_generations/cropped_cropped/24"
+#features_dir = r"../../generations/changemap_generations/cropped_cropped/result_20_cut_1_png"
+#features_dir = r"../../generations/changemap_generations/cropped_resized/result_20_cut_1_png"
 
 """ Features directory of the generated registered by deformation (This images are .jpg)"""
-#features_dir = r"../../generations/deformation_generations/registered/24"
-features_dir = r"../../generations/deformation_generations/deformations/24"
+features_dir = r"../../generations/deformation_generations/registered/20"
+#features_dir = r"../../generations/deformation_generations/deformations/24"
 
 
 #Only available for the brandao generator, timesteps
@@ -43,6 +39,7 @@ frames = []
 
 for filename in os.listdir(features_dir):
     if filename.endswith(".png"):
+    #if filename.endswith(".jpg"):
         img = Image.open(os.path.join(features_dir, filename))
         #width, height = img.size
         #img = img.resize((res, res)).convert('L') # Just keeping this line in case we want to change image sizes
@@ -53,7 +50,7 @@ for filename in os.listdir(features_dir):
 
 features = np.stack(frames, axis=0)
 #features = np.reshape(features, (470, res, res, 1)) # Artificially replicating pixel values as SINDy is a bit dumb
-features = np.reshape(features, (49, res, res, 1)) # Artificially replicating pixel values as SINDy is a bit dumb
+features = np.reshape(features, (50, res, res, 1)) # Artificially replicating pixel values as SINDy is a bit dumb
 spatial_grid_x, spatial_grid_y = np.meshgrid(np.arange(res), np.arange(res))
 spatial_grid = np.stack((spatial_grid_x, spatial_grid_y), axis=-1)
 print("Spatial grid shape:", np.shape(spatial_grid))
@@ -99,7 +96,7 @@ dt = np.arange(0, x.shape[-2]*0.05, 0.05)
 model = ps.SINDy(feature_library=lib, optimizer=opt, feature_names=["u"])
 
 x_dot = model.differentiate(x=x)  ## check pysindy.pi function differentiate for adjustment of timestep, not automatic.
-plt.imshow(x_dot[1,:,:,0], cmap='gray')
+plt.imshow(x_dot[20,:,:,0], cmap='gray')
 plt.colorbar(label='Intensity')
 plt.show()
 
